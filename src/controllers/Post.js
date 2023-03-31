99c6e09ddf95d1943a6e67c1823ac8eb4e08d480
Sergio Suárez <serrsua.88@gmail.com>
2023-03-31 14:19:50 -0300

    Cambios provisorios para que funcione el front

--- src/controllers/Post.js
+++ src/controllers/Post.js
@@ -1,89 +1,95 @@
-const { conn, Post, User, Tag, Op } = require('../db');
+const { conn, Post, User, Tag, Op } = require("../db");
 
 async function getAllPost(req, res) {
-    const { title, username, tag } = req.query
-    let options = {
-        include: [User, Tag]
+  const { title, username, tag } = req.query;
+  let options = {
+    include: [User, Tag],
+  };
+  let AND = [];
+  try {
+    if (title) {
+      AND.push({
+        title: {
+          [Op.like]: `%${title[0].toUpperCase() + title.slice(1)}%`,
+        },
+      });
     }
-    let AND = []
-    try {
-        if (title) {
-            AND.push({
-                title: {
-                    [Op.like]: `%${title[0].toUpperCase() + title.slice(1)}%`
-                }
-            })
-        }
-        if (username) {
-            AND.push({
-                '$User.username$': {
-                    [Op.like]: `%${username}%`
-                }
-            })
-        }
-        if (tag) {
-            AND.push({
-                '$Tags.name$': {
-                    [Op.like]: `%${tag[0].toUpperCase() + tag.slice(1)}%`
-                }
-            })
-        }
-        if (AND.length) {
-            options = {
-                ...options,
-                where: {
-                    [Op.and]: AND
-                }
-            }
-        }
-        const posts = await conn.model('Post').findAll(options)
-        return res.status(200).json(posts)
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send(error.message);
+    if (username) {
+      AND.push({
+        "$User.username$": {
+          [Op.like]: `%${username}%`,
+        },
+      });
     }
-};
+    if (tag) {
+      AND.push({
+        "$Tags.name$": {
+          [Op.like]: `%${tag[0].toUpperCase() + tag.slice(1)}%`,
+        },
+      });
+    }
+    if (AND.length) {
+      options = {
+        ...options,
+        where: {
+          [Op.and]: AND,
+        },
+      };
+    }
+    const posts = await conn.model("Post").findAll(options);
+    return res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error.message);
+  }
+}
 
 async function getPostById(req, res) {
-    console.log('Ruta Post a ID');
-    const { id } = req.params;
-    try {
-        const post = await conn.model('Post').findByPk(id);
-        if (post) {
-            return res.status(200).json(post);
-        } else {
-            return res.status(404).send('No se encontro el post');
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send(error.message);
+  console.log("Ruta Post a ID");
+  const { id } = req.params;
+  try {
+    const post = await conn.model("Post").findByPk(id);
+    if (post) {
+      return res.status(200).json(post);
+    } else {
+      return res.status(404).send("No se encontro el post");
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error.message);
+  }
 }
 
 const createPost = async (req, res) => {
-    const { title, description, file, userId, nameTag, fileTag } = req.body;
+  const { title, description, file, userId, nameTag, fileTag } = req.body;
 
-    try {
-        if (title && description && file && userId && nameTag) {
-            const newPost = await conn.model('Post').create({ title: title, description: description, file: file, UserId: userId })
-            const [tag, _] = await conn.model('Tag').findOrCreate({
-                name: nameTag,
-                where: {
-                    name: nameTag
-                }
-            })
-            newPost.addTag(tag)
-            return res.status(200).send("el post fue creado con exito")
-        }
-        return res.status(400).json({ error: "faltan datos" })
-    } catch (e) {
-        return res.status(400).send(e.message)
+  try {
+    if (title && description) { // borro provisoriamente file, userId y nameTag
+      const newPost = await conn
+        .model("Post")
+        .create({
+          title: title,
+          description: description,
+          file: file,
+          UserId: userId,
+        });
+      /* const [tag, _] = await conn.model("Tag").findOrCreate({        // comento provisoriamente el nameTag
+        name: nameTag,
+        where: {
+          name: nameTag,
+        },
+      });
+      newPost.addTag(tag); */
+      return res.status(200).send("el post fue creado con exito");
     }
-
-}
+    return res.status(400).json({ error: "faltan datos" });
+  } catch (e) {
+    return res.status(400).send(e.message);
+  }
+};
 
 module.exports = {
-    getAllPost,
-    getPostById,
-    createPost
-}
+  getAllPost,
+  getPostById,
+  createPost,
+};
