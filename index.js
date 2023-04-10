48110558b8e6b62cae1614b659ab096ef40e54bc
juncos22 <juncosnicolas36@gmail.com>
2023-04-09 22:52:36 -0300

    cambiando cadena de conexion

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function createPosts() {
 
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(async () => {
+conn.sync({ force: false }).then(async () => {
   console.log('db connected')
   createUsers()
   createPosts()
