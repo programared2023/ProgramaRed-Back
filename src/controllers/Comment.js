29b57f7408dae35d282e06a823ef339cf95cf24e
juncos22 <juncosnicolas36@gmail.com>
2023-04-14 08:36:52 -0300

    update y delete comments

--- src/controllers/Comment.js
+++ src/controllers/Comment.js
@@ -19,6 +19,39 @@ const saveComment = async (req, res) => {
     }
 }
 
+const updateComment = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { comment } = req.body
+        const [updated] = await conn.model("Comment").update({
+            comment: comment
+        }, { where: { id: id } })
+        console.log(`${updated} comment updated`);
+        return res.status(200).send("Comentario actualizado")
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error.message)
+    }
+}
+
+const deleteComment = async (req, res) => {
+    try {
+        const { id, postId } = req.body
+        const comment = await conn.model("Comment").findOne({ where: { id: id } })
+        const post = await conn.model("Post").findOne({ where: { id: postId } })
+        if (comment && post) {
+            post.removeComment(comment)
+            await comment.destroy()
+        }
+        return res.status(200).send("Comentario eliminado")
+    } catch (error) {
+        console.log(error);
+        return res.status(200).send(error.message)
+    }
+}
+
 module.exports = {
-    saveComment
+    saveComment,
+    updateComment,
+    deleteComment
 }
